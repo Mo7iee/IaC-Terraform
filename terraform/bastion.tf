4f5fcc523968d4a013f1bde8e0d950d827b3bdc7
Mo7iee <mohie707034@gmail.com>
2025-10-27 21:14:39 +0300

    subnets + jump server

---
+++ terraform/bastion.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "bastion" {
+  ami                    = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI (us-east-1)
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_sub1.id  
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  key_name               = "your-keypair-name"        # replace with your SSH key   
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "bastion-host"
+  }
+}
---
+++ terraform/subnet.tf
@@ -0,0 +1,37 @@
+# Public Subnet 1
+resource "aws_subnet" "public_sub1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "192.168.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = { Name = "public-subnet-a" }
+}
+
+# Public Subnet 2
+resource "aws_subnet" "public_sub2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "192.168.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = { Name = "public-subnet-b" }
+}
+
+# Private Subnet 1
+resource "aws_subnet" "private_sub1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "192.168.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = { Name = "private-subnet-a" }
+}
+
+# Private Subnet 2
+resource "aws_subnet" "private_sub2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "192.168.4.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = { Name = "private-subnet-b" }
+}
